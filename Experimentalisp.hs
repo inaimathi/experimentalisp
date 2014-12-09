5cfe83f809e52037300a31693367cbcc755a11c1
inaimathi <inaimathi@self>
2014-12-08 20:28:43 -0500

    -Very VERY minor cleanup in the Haskell version

--- Experimentalisp.hs
+++ Experimentalisp.hs
@@ -13,9 +13,8 @@ global_env = fromList empty
              , ("=", lisp_prim ["a", "b"] (\env [a, b] -> (Bool $ a == b, env)))
              , ("car", lisp_prim ["a"] (\env [Cell car _] -> (car, env)))
              , ("cdr", lisp_prim ["a"] (\env [Cell _ cdr] -> (cdr, env)))
-             , ("cons", lisp_prim ["a", "b"] cons)]
-    where cons env [a, b] = (Cell a b, env)
-          cons env args = error $ unlines ["CONS: ", show env, show args]
+             , ("cons", lisp_prim ["a", "b"] (\env [a, b] -> (Cell a b, env)))
+             ]
 
 main_loop :: Environment -> IO ()
 main_loop env = do putStr " >> "
