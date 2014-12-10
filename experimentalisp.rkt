f4b73915d32aba66cfd88b6f7c6b1298a76140e9
inaimathi <inaimathi@self>
2014-12-09 22:15:33 -0500

    -Moved all the prim macro and primitive definitions over to model.rkt to match the haskell version

--- experimentalisp.rkt
+++ experimentalisp.rkt
@@ -3,16 +3,6 @@
 
 (require "evaluator.rkt" "model.rkt")
 
-(define-syntax prim
-  (syntax-rules ()
-    [(prim name (a ...) body)
-     (bind! global-env 'name
-	    (primitive 
-	     '(a ...) (lambda (env)
-			(let ((a (cdr (lookup env 'a)))
-			      ...)
-			  body))))]))
-
 (define (loop)
   (display "EXP>> ")
   (print (exp-eval (read) global-env))
@@ -22,25 +12,8 @@
 (define (main)
   (displayln "experimentalisp 0.001")
 
-  ;;;;; Basic primitives
-  (bind! 
-   global-env 'the-env
-   (primitive '() (lambda (env) env)))
-  
-  (prim + (a b) (+ a b))
-  (prim - (a b) (- a b))
-  (prim / (a b) (/ a b))
-  (prim * (a b) (* a b))
-  (prim = (a b) (if (eq? a b) 'true 'false))
-  (prim car (a) (car a))
-  (prim cdr (a) (cdr a))
-  (prim cons (a b) (cons a b))
-  (prim print (thing) (begin (displayln thing) '()))
-
   ;;;;; Initial display
   (displayln " Base env:")
   (display " ") (displayln global-env)
 
   (loop))
-
-;; (require "experimentalisp.rkt") (main)
--- model.rkt
+++ model.rkt
@@ -5,35 +5,11 @@
 	 make-partial partial? complete? collapse
 	 arglist-of environment-of body-of
 
-	 global-env make-env extend-env lookup bind! re-bind!
+	 global-env extend-env lookup bind! re-bind!
 
 	 true? self-evaluating?)
 
-;;;;;;;;;; Environments
-(define (make-env) (list (make-hash)))
-(define global-env (make-env))
-(define (extend-env env)
-  (cons (make-hash) env))
-(define (lookup env exp)
-  (cond ((null? env) '())
-	((hash-has-key? (car env) exp)
-	 (cons exp (hash-ref (car env) exp)))
-	(else
-	 (lookup (cdr env) exp))))
-(define (bind! env name exp)
-  (if (hash-has-key? (car env) name)
-      (error (format "BIND: tried clobbering name '~a'...~%" name))
-      (hash-set! (car env) name exp))
-  env)
-(define (re-bind! env name exp)
-  (cond ((null? env) 
-	 (error (format "RE-BIND: symbol '~a' is unassigned~%" name)))
-	((hash-has-key? (car env) name)
-	 (hash-set! (car env) name exp))
-	(else
-	 (re-bind! (cdr env) name exp)))
-  env)
-
+;;;;;;;;;; Partial application
 (define (make-partial thing new-args)
   (letrec ((argl (arglist-of thing))
 	   (rec (lambda (ks vs)
@@ -86,6 +62,58 @@
 	((procedure? thing) (procedure-body thing))
 	((fexpr? thing) (fexpr-body thing))
 	((partial? thing) (partial-body thing))))
+
+;;;;;;;;;; Environments
+(define (extend-env env) (cons (make-hash) env))
+
+(define (lookup env exp)
+  (cond ((null? env) '())
+	((hash-has-key? (car env) exp)
+	 (cons exp (hash-ref (car env) exp)))
+	(else
+	 (lookup (cdr env) exp))))
+
+(define (bind! env name exp)
+  (if (hash-has-key? (car env) name)
+      (error (format "BIND: tried clobbering name '~a'...~%" name))
+      (hash-set! (car env) name exp))
+  env)
+
+(define (re-bind! env name exp)
+  (cond ((null? env) 
+	 (error (format "RE-BIND: symbol '~a' is unassigned~%" name)))
+	((hash-has-key? (car env) name)
+	 (hash-set! (car env) name exp))
+	(else
+	 (re-bind! (cdr env) name exp)))
+  env)
+
+(define-syntax prim
+  (syntax-rules ()
+    [(prim env name (a ...) body)
+     (bind! env 'name
+	    (primitive 
+	     '(a ...) (lambda (env)
+			(let ((a (cdr (lookup env 'a)))
+			      ...)
+			  body))))]))
+
+(define global-env 
+  (let ((env (list (make-hash))))
+    (bind! 
+     env 'the-env
+     (primitive '() (lambda (env) env)))
+    
+    (prim env + (a b) (+ a b))
+    (prim env - (a b) (- a b))
+    (prim env / (a b) (/ a b))
+    (prim env * (a b) (* a b))
+    (prim env = (a b) (if (eq? a b) 'true 'false))
+    (prim env car (a) (car a))
+    (prim env cdr (a) (cdr a))
+    (prim env cons (a b) (cons a b))
+    (prim env print (thing) (begin (displayln thing) '()))
+    env))
 
 ;;;;;;;;;; Basics
 (define (true? thing)
