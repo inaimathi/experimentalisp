c41218c14c5532ca2bd3b5560b88365ad9f589dd
inaimathi <inaimathi@self>
2014-12-10 22:53:03 -0500

    -Re-indenting Experimentascript

--- Experimentascript.hs
+++ Experimentascript.hs
@@ -8,9 +8,10 @@ import Haste
 import Data.IORef
 
 prependContent :: ElemID -> String -> IO ()
-prependContent id newContent = withElem id (\e -> do cont <- getProp e "innerHTML"
-                                                     setProp e "innerHTML" $ concat [newContent, "\n\n", cont]
-                                                     return ())
+prependContent id newContent = 
+    withElem id (\e -> do cont <- getProp e "innerHTML"
+                          setProp e "innerHTML" $ concat [newContent, "\n\n", cont]
+                          return ())
 
 escape :: String -> String
 escape [] = []
@@ -19,21 +20,23 @@ escape ('>':rest) = "&gt;" ++ escape rest
 escape (c:rest) = c : (escape rest)
 
 eval_contents :: Elem -> Environment -> IO Environment
-eval_contents inp env = do Just val <- getValue inp
-                           case lisp_read val of
-                             Right exp -> let (res, env') = Evaluator.eval exp env
-                                          in do prependContent "repl-log" . escape $ concat [val, "\n   => ", show res]
-                                                return env'                               
-                             _ -> do prependContent "repl-log" "Read error ..."
-                                     return env
+eval_contents inp env = 
+    do Just val <- getValue inp
+       case lisp_read val of
+         Right exp -> let (res, env') = Evaluator.eval exp env
+                      in do prependContent "repl-log" . escape $ concat [val, "\n   => ", show res]
+                            return env'                               
+         _ -> do prependContent "repl-log" "Read error ..."
+                 return env
 
 main :: IO ()
 main = withElem "repl-input" $ \inp -> 
        do env_ref <- newIORef global_env 
-          onEvent inp OnKeyPress $ \k -> case k of
-                                           13 -> do env <- readIORef env_ref 
-                                                    env' <- eval_contents inp env
-                                                    _ <- writeIORef env_ref env'
-                                                    return ()
-                                           _ -> return ()
+          onEvent inp OnKeyPress $ \k -> 
+              case k of
+                13 -> do env <- readIORef env_ref 
+                         env' <- eval_contents inp env
+                         _ <- writeIORef env_ref env'
+                         return ()
+                _ -> return ()
           return ()
