a624a55310443311ba7fbfb20312ee5e19de272d
inaimathi <inaimathi@self>
2014-12-11 21:09:14 -0500

    -Multi-line prompt now working thanks to pipes

--- Experimentalisp.hs
+++ Experimentalisp.hs
@@ -4,22 +4,67 @@ import Model
 import Reader
 import Evaluator
 
+import Pipes
+import Control.Monad (unless, when)
+import Control.Exception (try, throwIO)
+import qualified GHC.IO.Exception as G
 import System.IO
+import Data.Char (isSpace)
 
-main_loop :: Environment -> IO ()
-main_loop env = do putStr "EXP>> "
-                   hFlush stdout
-                   ln <- getLine
-                   case lisp_read ln of
-                     Right res -> let evaled = eval res env
-                                      env' = case evaled of
-                                               (Res _) -> env
-                                               (Mod _ e) -> e
-                                  in do putStrLn . show $ res_of evaled
-                                        main_loop env'
-                     _ -> do putStrLn "READ ERROR"
-                             main_loop env
+strip :: String -> String
+strip = takeWhile (not . isSpace) . dropWhile isSpace
+
+stdinLn :: Producer String IO ()
+stdinLn = do
+  eof <- lift isEOF
+  unless eof $ do
+            str <- lift getLine
+            yield str
+            stdinLn
+
+reader :: (Monad m) => [String] -> Pipe String LispVal m ()
+reader acc = do ln <- await
+--                when ("" == ln) $ yield $ Sym "EOL"
+--                yield $ Str ln
+                case strip ln of
+                  ":c" -> reader []
+                  _ -> case lisp_read . unlines $ reverse (ln:acc) of
+                         Right res -> do yield res
+                                         reader []
+                         Left _ -> reader $ ln : acc
+
+evaluator :: (Monad m) => Environment -> Pipe LispVal LispVal m ()
+evaluator env = do exp <- await
+                   let evaled = eval exp env
+                       env' = case evaled of
+                                (Res _) -> env
+                                (Mod _ e) -> e
+                   do yield $ res_of evaled
+                      evaluator env'
+
+prompt :: Show a => Consumer a IO ()
+prompt = do lift $ putStr "\nEXP>> "
+            lift $ hFlush stdout
+            msg <- await
+            x <- lift $ try $ putStrLn $ show msg
+            case x of
+              Left e@(G.IOError { G.ioe_type = t}) ->
+                  lift $ unless (t == G.ResourceVanished) $ throwIO e
+              Right () -> prompt
+                    
+
+toStdout :: Show a => Consumer a IO ()
+toStdout = do
+  msg <- await
+  x   <- lift $ try $ putStr $ show msg
+  case x of
+    Left e@(G.IOError { G.ioe_type = t}) ->
+           lift $ unless (t == G.ResourceVanished) $ throwIO e
+    Right () -> do lift $ hFlush stdout
+                   toStdout
 
 main :: IO ()
 main = do putStrLn $ "experimentaLISP v0.00001"
-          main_loop global_env
+          runEffect $ stdinLn >-> 
+                    reader [] >-> evaluator global_env >-> 
+                    prompt
